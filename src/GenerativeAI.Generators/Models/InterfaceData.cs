ec63d1815127b1af4b92038ad762b0a7bd1f00c4
Gunpal Jain <gunpal5@hotmail.com>
2025-02-11 01:57:41 +0530

    remove: removed code generator with which is not needed in new version.

--- src/GenerativeAI.Generators/Models/InterfaceData.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-
-namespace GenerativeAI.Generators.Models;
-
-public readonly record struct InterfaceData(
-    string Namespace,
-    string Name,
-    IReadOnlyCollection<MethodData> Methods);
