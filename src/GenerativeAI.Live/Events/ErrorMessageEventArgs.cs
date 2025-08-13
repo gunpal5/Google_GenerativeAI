75ea2e74e7cfdc299fe63fb6383471772cda623d
Lukasz Fliegel <lukasz_fliegel@epam.com>
2025-08-13 13:26:05 +0200

    Added error message porpagation to client. In case of quote extension, SDK disconnected client without any info why.

---
+++ src/GenerativeAI.Live/Events/ErrorMessageEventArgs.cs
@@ -0,0 +1,20 @@
+﻿namespace GenerativeAI.Live.Events;
+
+/// <summary>
+/// Provides error message event data.
+/// </summary>
+public class ErrorMessageEventArgs : EventArgs
+{
+    /// <summary>
+    /// Gets the payload of the received message.
+    /// </summary>
+    public string ErrorMessage { get; }
+
+    /// <devdoc>
+    ///    Initializes a new instance of the class.
+    /// </devdoc>
+    public ErrorMessageEventArgs(string errorMessage)
+    {
+        ErrorMessage = errorMessage;
+    }
+}
